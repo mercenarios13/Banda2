b9e8efdf1df9cc511dbf882adf69e838b6b60c59
vitor alexandre <165023350+mercenarios13@users.noreply.github.com>
2024-11-27 13:29:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    // Display success message after form submission
     document.getElementById('formMessage').style.display = 'block';
-    
-    // Clear the form fields after 3 seconds
+
     setTimeout(() => {
         document.getElementById('formMessage').style.display = 'none';
         document.getElementById('contactForm').reset();
